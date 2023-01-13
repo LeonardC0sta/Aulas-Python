31d7c23b7ae161f2e0fac91a591a17257ab5ea04
Leonardo Costa <leode98@hotmail.com>
2023-01-13 13:47:37 -0300

    Adicinando exercício da Aula1

---
+++ Aula1.py
@@ -0,0 +1,15 @@
+"""
+Escrever o que quiser com isso, tenho que melhor a digitação nesse teclado
+Sempre usar para colocar o que foi feito na linha de codigo 
+Usar aspas duplas ou simples, para abrir e para fechar 
+
+# Permite escrever um comentário 
+print(123)
+print(456)
+"""
+# Imprime um número
+# print(123) 
+""" Estou adorando Python """
+print('Agora sou dev.')
+#
+
---
+++ Aula3.py
@@ -0,0 +1,11 @@
+"""
+Conversão, tipos de coerção 
+type convertion, typecasting, coercion 
+é o ato de converte um tipo em outro 
+tipos imutáveis e primítivos 
+str, int, float, bool 
+"""
+print('1', type('1'))
+print(type(int('1') + 1)) # conersão da var '1' para interiro 
+print(type(float('1') + 1)) # conersão da var '1' para float = retorna um número float
+print(int('8') + 1)
